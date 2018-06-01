5356c7088776d3ac7b6285b53a9c168bd21e3d4f
knievk <39812415+knievk@users.noreply.github.com>
2018-06-01 11:32:17 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# node.nigar.io
---
+++ static/game.js
@@ -0,0 +1,70 @@
+var socket = io();
+socket.on('message', function(data) {
+  console.log(data);
+});
+var movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
+}
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = true;
+      break;
+    case 87: // W
+      movement.up = true;
+      break;
+    case 68: // D
+      movement.right = true;
+      break;
+    case 83: // S
+      movement.down = true;
+      break;
+  }
+});
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = false;
+      break;
+    case 87: // W
+      movement.up = false;
+      break;
+    case 68: // D
+      movement.right = false;
+      break;
+    case 83: // S
+      movement.down = false;
+      break;
+  }
+});socket.emit('new player');
+var lastUpdateTime = (new Date()).getTime();
+setInterval(function() {
+  // code ...
+  var currentTime = (new Date()).getTime();
+  var timeDifference = currentTime - lastUpdateTime;
+  player.x += 5 * timeDifference;
+  lastUpdateTime = currentTime;
+}, 1000 / 60);
+var canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+socket.on('state', function(players) {
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
+io.on('connection', function(socket) {
+  // other handlers ...
+  socket.on('disconnect', function() {
+    // remove disconnected player
+  });
+});
